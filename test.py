a2a7b6d9d6ed76ead9ed707dac96e46903514145
rfa-legion <rfa.iyang3@gmail.com>
2026-03-01 01:50:19 +0700

    clean repo

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ plt.style.use(['science','no-latex','ieee'])
 #import warnings
 
 e, kB = constants.e, constants.Boltzmann
-phi0, d0, T0, alpha0 =1e16 ,8e-4 ,3e2, 5e3
+phi_0, d, T, alpha =1e16 ,8e-4 ,3e2, 5e3
 
-def extended_shockley(V, m, n_0, tau_e, D_e, phi_0=phi0, d=d0, T=T0, alpha=alpha0):
-    """ extended shockley equation is an ideal shockley equation derive from
+def extended_shockley(V, m, n_0, tau_e, D_e):#, phi_0=phi0, d=d0, T=T0, alpha=alpha0):
+    """ extended shockley equation is an shockley like's equation derive from maxwell equation?
 
     Args:
         V (float): voltage
@@ -25,16 +25,32 @@ def extended_shockley(V, m, n_0, tau_e, D_e, phi_0=phi0, d=d0, T=T0, alpha=alpha
     Returns:
         float: total current density
     """
-    L = np.sqrt(D_e *tau_e)
-    jsc = e*phi_0*L^2 *alpha/(1-L^2 *alpha^2) *(-alpha + 1/L*np.tanh(d/L) +alpha *np.exp(-alpha*d)/(np.cosh(d/L)))
-    j_0 = e*n_0*L/tau_e*np.tanh(d/L)* (np.exp(e*V/(m*kB*T)) )
-    return jsc-j_0
+    # Diffusion length L
+    L = np.sqrt(D_e * tau_e)
+    
+    # Pre-factor for the first large bracket
+    denom = 1 - (L**2 * alpha**2)
+    term1_prefix = (e * phi_0 * L**2 * alpha) / denom
+    
+    # Contents of the square bracket []
+    bracket = -alpha + (1/L * np.tanh(d/L)) + (alpha * np.exp(-alpha * d) / np.cosh(d/L))
+    
+    # short circuit current (density) , can be estimate from I with V ~ 0
+    j_sc = term1_prefix * bracket
+    
+    # sqrt(D_e/tau_e) is the diffusion velocity
+    j_0 = e * n_0 * np.sqrt(D_e / tau_e) * np.tanh(d/L)
+    
+    # Equation 20: j = j_sc - j_0 * (exp(eV/mkT) - 1)
+    j_total = j_sc - j_0 * (np.exp((e * V) / (m * kB * T)) - 1)
+    
+    return j_total
 
 file = "./data/shakinah.xlsx"
 data = [pd.read_excel(file,sheet,header=2)  for sheet in pd.ExcelFile(file).sheet_names]
-jsc = data[0].iloc[data[0]["WE(1).Potential (V)"].abs().idxmin(), 2]
+jsc_from_data = data[0].iloc[data[0]["WE(1).Potential (V)"].abs().idxmin(), 2]
 I,V =data[0]["mili A"], data[0]["WE(1).Potential (V)"]
 
-initial_geuss=[]
-low_lim, up_lim = [],[]
-prams, covs = optimize.curve_fit(extended_shockley,V,I,p0=initial_geuss, bounds=(low_lim, up_lim))
+initial_geuss = [ 4.5, 1e11, 1e-2, 1e-4] # from reference
+low_lim, up_lim = [1, 0, 0, 0],[10, np.inf, np.inf, np.inf]
+prams, covs = optimize.curve_fit(extended_shockley,V,I)#,p0=initial_geuss), bounds=(low_lim, up_lim),full_output=True)
